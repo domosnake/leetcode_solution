af5ef217f67aed9796766a9a1960d763e91ac41c
domosnake <domosanke@hotmail.com>
2020-09-16 12:12:06 -0400

    Added build bst from inorder

--- 109.convert-sorted-list-to-binary-search-tree.py
+++ 109.convert-sorted-list-to-binary-search-tree.py
@@ -23,7 +23,7 @@ class TreeNode:
 
 
 class Solution:
-    def sortedListToBST(self, head: ListNode) -> TreeNode:
+    def sortedListToBST_array(self, head: ListNode) -> TreeNode:
         # convert to array and build BST
         # as find center in array is O(1)
         if head is None:
@@ -33,10 +33,10 @@ class Solution:
         while cur:
             list.append(cur.val)
             cur = cur.next
-        root = self.buildBST(list, 0, len(list) - 1)
+        root = self.buildBST_array(list, 0, len(list) - 1)
         return root
 
-    def buildBST(self, list: List[int], lo: int, hi: int) -> TreeNode:
+    def __buildBST_array(self, list: List[int], lo: int, hi: int) -> TreeNode:
         if lo > hi:
             return None
         # Middle element forms the root.
@@ -49,5 +49,49 @@ class Solution:
         node.right = self.buildBST(list, mid + 1, hi)
         return node
 
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        if head is None:
+            return None
+        # build BST from inorder
+        length = 0
+        cur = head
+        while cur:
+            length += 1
+            cur = cur.next
 
+        # build BST by simulating inorder traversal
+        cur = head
+        root = self.__buildBST_inorder(cur, 0, length - 1)
+        return root
+
+    def __buildBST_inorder(self, curListNode: ListNode, lo: int, hi: int) -> TreeNode:
+        if lo > hi:
+            return None
+
+        mid = (lo + hi) // 2
+
+        # simulate inorder traversal
+        # recursively form the left subtree
+        left = self.__buildBST_inorder(curListNode, lo, mid - 1)
+
+        # process the parent node
+        node = TreeNode(curListNode.val)
+        node.left = left
+
+        curListNode = curListNode.next
+
+        # recursively form the right subtree
+        node.right = self.__buildBST_inorder(curListNode, mid + 1, hi)
+        return node
+
+
+s = Solution()
+head = ListNode(1)
+head.next = ListNode(4)
+head.next.next = ListNode(7)
+head.next.next.next = ListNode(11)
+head.next.next.next.next = ListNode(20)
+head.next.next.next.next.next = ListNode(38)
+a = s.sortedListToBST(head)
+print(a)
 # @lc code=end
